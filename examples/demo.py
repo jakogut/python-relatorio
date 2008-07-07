1d5b555986cc4e1d4b2df26be40da6b102efa65b
Nicolas Evrard <nicoe@openhex.org>
2008-07-07 09:30:37 +0000

    - Added the examples directory

---
+++ examples/demo.py
@@ -0,0 +1,55 @@
+import operator
+import relatorio
+
+class Invoice(dict):
+
+    @property
+    def total(self):
+        return reduce(operator.add, (l['amount'] for l in self['lines']), 0)
+
+    @property
+    def vat(self):
+        return self.total * 0.21
+
+
+repos = relatorio.ReportRepository()
+repos.add_report(Invoice, 'application/vnd.oasis.opendocument.text',
+                 'basic.odt', report_name='basic')
+repos.add_report(Invoice, 'application/vnd.oasis.opendocument.text',
+                 'invoice.odt', report_name='complicated')
+repos.add_report(Invoice, 'application/vnd.oasis.opendocument.text',
+                 'test.odt', report_name='test')
+
+inv = Invoice(customer={'name': 'John Bonham',
+                        'address': {'street': 'Smirnov street',
+                                    'zip': 1000,
+                                    'city': 'Montreux'}},
+              lines=[{'item': {'name': 'Vodka 70cl',
+                               'reference': 'VDKA-001',
+                               'price': 10.34},
+                      'quantity': 7,
+                      'amount': 7*10.34},
+                     {'item': {'name': 'Cognac 70cl',
+                               'reference': 'CGNC-067',
+                               'price': 13.46},
+                      'quantity': 12,
+                      'amount': 12*13.46},
+                     {'item': {'name': 'Sparkling water 25cl',
+                               'reference': 'WATR-007',
+                               'price': 0.4},
+                      'quantity': 1,
+                      'amount': 0.4},
+                     {'item': {'name': 'Good customer',
+                               'reference': 'BONM-001',
+                               'price': -20},
+                      'quantity': 1,
+                      'amount': -20},
+                    ],
+              id='MZY-20080703',
+              status='late')
+
+
+_, basic_report = repos.reports[Invoice]['basic']
+file('bonham_basic.odt', 'w').write(basic_report(inv).getvalue())
+_, report = repos.reports[Invoice]['complicated']
+file('bonham_complicated.odt', 'w').write(report(inv).getvalue())
