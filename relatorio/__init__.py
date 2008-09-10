c2efa1b6d747677ff1d44c42ad23e4be54b0e0c5
Nicolas ?vrard <nicoe@openhex.org>
2008-09-10 11:20:11 +0200

    Preparation for the release.

--- relatorio/__init__.py
+++ relatorio/__init__.py
@@ -1,4 +1,15 @@
+"""
+relatorio
+=========
+
+A templating library which provides a way to easily output all kind of
+different files (odt, ods, png, svg, ...). Adding support for more filetype is
+easy: you just have to create a plugin for this.
+
+relatorio also provides a report repository allowing you to link python objects
+and report together, find reports by mimetypes/name/python objects.
+"""
 from relatorio.reporting import MIMETemplateLoader, ReportRepository, Report
 MIMETemplateLoader.load_template_engines()
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
--- setup.py
+++ setup.py
@@ -8,21 +8,13 @@ setup(
     author="Nicolas Evrard",
     author_email="nicoe@openhex.org",
     description="A templating library able to output odt and pdf files",
-    long_description="""
-relatorio
-========
-
-A templating library which provides a way to easily output odt and pdf file.
-
-relatorio also provides a report repository allowing you to link python objects
-and report together, find reports by mimetypes/name/pyhon objects.
-""",
+    long_description=relatorio.__doc__,
     license="GPL License",
     version=relatorio.__version__,
     packages=find_packages(exclude=['relatorio.tests', 'examples']),
     install_requires=[
         "Genshi >= 0.5",
-        "lxml >= 2.0.0"
+        "lxml >= 1.3.6"
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -40,5 +32,6 @@ and report together, find reports by mimetypes/name/pyhon objects.
             "pdf = relatorio.templates.pdf:Template",
             "text = genshi.template:TextTemplate",
             "xml = genshi.template:MarkupTemplate",
+            "chart = relatorio.templates.chart:Template",
         ],
     })
