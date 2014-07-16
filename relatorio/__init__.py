ba1d8b0e78f677dac3b22f2c860aca54720addf8
C?dric Krier <ced@b2ck.com>
2014-07-16 16:26:43 +0200

    Use relative import
    issue12

--- relatorio/__init__.py
+++ relatorio/__init__.py
@@ -9,7 +9,7 @@ easy: you just have to create a plugin for this.
 relatorio also provides a report repository allowing you to link python objects
 and report together, find reports by mimetypes/name/python objects.
 """
-from relatorio.reporting import MIMETemplateLoader, ReportRepository, Report
-import templates
+from .reporting import MIMETemplateLoader, ReportRepository, Report
+from . import templates
 
 __version__ = '0.6.1'
